2654273d76b96fc9aa879b8706e818293f007b60
Pasquale Russo <pasquale01.russo@edu.unife.it>
2025-04-14 14:42:24 +0200

    Primo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+export const routes: Routes = [
+    {path: "login", component: LoginComponent},
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: HomeComponent},
+    {path: "about_us", component:   AboutUsComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "contacts", component: ContactsComponent},
+
+];
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [RouterOutlet],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
