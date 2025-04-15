1809fc20c5bacb9b23214addda6f55a69eacf4b2
Pasquale Russo <pasquale01.russo@edu.unife.it>
2025-04-15 18:35:07 +0200

    aggiunta cartella area personale

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AreaPersonaleComponent } from './area-personale/area-personale.component';
 
 export const routes: Routes = [
     {path: "login", component: LoginComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path: "about_us", component:   AboutUsComponent},
     {path: "register", component: RegisterComponent},
     {path: "contacts", component: ContactsComponent},
+    {path: "area-personale", component: AreaPersonaleComponent}
 
 ];
---
+++ src/app/area-personale/area-personale.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-personale',
+  imports: [],
+  templateUrl: './area-personale.component.html',
+  styleUrl: './area-personale.component.css'
+})
+export class AreaPersonaleComponent {
+
+}
