e7dd90fa413b31decf5af0fe9f6827138d9a733e
IMRAN WAHEED <imran.waheed@edu.unife.it>
2025-04-20 15:03:14 +0200

    back end

---
+++ back-end/src/test/java/com/BankU/BankU_Backend/BankUBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankU.BankU_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankUBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
